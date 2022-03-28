582aab235f8ce3ea01cb00d387a5b3e8aa6dead0
Victor Le Roy Matos <victorlrmatos@gmail.com>
2022-03-28 19:54:19 -0300

    Quinto envio
    . Comecando a implementacao da UI
    
    OBS:
    Descobrir como fazer um monitor interativo (se n conseguir fazer uma cena separada)

---
+++ Victor - Vaga Unity/Assets/Scripts/Canvas/Menu/MenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    void RestartButton() {
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+    void TelaButton () {
+
+    }
+
+    void TeslaButton () {
+        
+    }
+}
